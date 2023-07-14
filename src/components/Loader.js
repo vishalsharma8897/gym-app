ba997b3d30c67b778d4715a8c9c8487088aeb275
Vishal <vishalsharma19052001@gmail.com>
2023-07-14 17:16:23 +0530

    intial commit

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+import { BounceLoader } from 'react-spinners';
 
 const Loader = () => (
   <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <InfinitySpin color="grey" />
+    <BounceLoader color="grey" />
   </Stack>
 );
 
